485ce3ba8fe05df7bb0d7aab7934ce9f2b804c41
Kelvin Galabuzi Lukoda <kelvingalabuzi@gmail.com>
2022-03-09 23:36:44 +0300

    Update aws_cloudwatch_log_stream.tf

--- aws_cloudwatch_log_stream.tf
+++ aws_cloudwatch_log_stream.tf
@@ -1,4 +1,4 @@
-/*resource "aws_cloudwatch_log_group" "ebs_log_group" {
+resource "aws_cloudwatch_log_group" "ebs_log_group" {
   name = "ebs_log_group"
 }
 
@@ -6,4 +6,3 @@ resource "aws_cloudwatch_log_stream" "ebs_log_stream" {
   name           = "ebs_log_stream"
   log_group_name = aws_cloudwatch_log_group.ebs_log_group.name
 }
-*/
