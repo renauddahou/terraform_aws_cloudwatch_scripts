74e5ac77483d0debfefbbc9960c7d08df8aef5b2
Kelvin Galabuzi Lukoda <kelvingalabuzi@gmail.com>
2022-03-09 23:37:36 +0300

    Update aws_cloudwatch_metric_alarm.tf

--- aws_cloudwatch_metric_alarm.tf
+++ aws_cloudwatch_metric_alarm.tf
@@ -1,4 +1,3 @@
-/*
 resource "aws_cloudwatch_metric_alarm" "EC2_CPU_Usage_Alarm" {
   alarm_name                = "EC2_CPU_Usage_Alarm"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
@@ -10,4 +9,3 @@ resource "aws_cloudwatch_metric_alarm" "EC2_CPU_Usage_Alarm" {
   threshold                 = "70"
   alarm_description         = "This metric monitors ec2 cpu utilization exceeding 70%"
 }
-*/
